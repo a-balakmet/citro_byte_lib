bd4be5ad4d07a06fe3b6bb449806dd6dc55ea94c
andrey <a.balakhmet@gmail.com>
2023-02-05 18:02:50 +0600

    Shared Preferences redevelopment 4

--- lib/shared_prefs/prefs_helper.dart
+++ lib/shared_prefs/prefs_helper.dart
@@ -1,25 +1,28 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PrefsHelper<T> {
   static Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 
-  Future<void> set<R>(String key, T value) async {
+  Future<void> setString(String key, String value) async {
     final p = await prefs;
-    if (T is bool) {
-      p.setBool(key, value as bool);
-    } else if (T is int) {
-      p.setInt(key, value as int);
-    } else if (T is double) {
-      p.setDouble(key, value as double);
-    } else if (T is String) {
-      p.setString(key, value as String);
-    } else {
-      log("Prefs error while SET key $key");
-      throw UnimplementedError();
-    }
+    p.setString(key, value);
+  }
+
+  Future<void> setBool(String key, bool value) async {
+    final p = await prefs;
+    p.setBool(key, value);
+  }
+
+  Future<void> setInt(String key, int value) async {
+    final p = await prefs;
+    p.setInt(key, value);
+  }
+
+  Future<void> setDouble(String key, double value) async {
+    final p = await prefs;
+    p.setDouble(key, value);
   }
 
   Future<String> getString(String key) async {
@@ -44,26 +47,6 @@ class PrefsHelper<T> {
   Future<double> getDouble(String key) async {
     final p = await prefs;
     return p.getDouble(key) ?? 0.0;
-  }
-
-  Future<R> get<R>(String key) async {
-    final p = await prefs;
-    if (R is bool) {
-      return (p.getBool(key) ?? false) as R;
-    } else if (R is int) {
-      return (p.getInt(key) ?? 0) as R;
-    } else if (R is double) {
-      return (p.getDouble(key) ?? 0) as R;
-    } else if (R is String) {
-      if (key == 'locale') {
-        return (p.getString(key) ?? "ru") as R;
-      } else {
-        return (p.getString(key) ?? "") as R;
-      }
-    } else {
-      log("Prefs error while GET key $key");
-      throw UnimplementedError();
-    }
   }
 
   Future<R?> getClass<R>(String key, Function(T) converter) async {
