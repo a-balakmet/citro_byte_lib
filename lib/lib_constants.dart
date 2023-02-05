b03a64ff57f889c456812649afb78d0b6c7402f9
andrey <a.balakhmet@gmail.com>
2023-02-05 12:24:43 +0600

    empty error added to constants

--- lib/lib_constants.dart
+++ lib/lib_constants.dart
@@ -1,7 +1,11 @@
+import 'package:citro_byte_lib/resource/error.dart';
+
 class PkgConstants {
   static const String fullZPattern = "yyyy-MM-dd'T'HH:mm:ss'Z'";
   static const String fullPattern = "yyyy-MM-dd'T'HH:mm:ss";
   static const String backPattern = "yyyy-MM-dd";
 
   RegExp acceptableNameChars = RegExp(r'^[a-zA-Zа-яА-Я әғқңөұүhіӘҒҚҢӨҰҮҺІ-]+$');
+
+  ErrorMessage emptyError = ErrorMessage(backEndCode: '', error: '', isTranslatable: false);
 }
