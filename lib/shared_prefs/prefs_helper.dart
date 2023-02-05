ecb879aa9f5c3b1192b1c7cf068d9838357cb7ce
andrey <a.balakhmet@gmail.com>
2023-02-05 17:02:39 +0600

    Shared Preferences redevelopment

--- lib/shared_prefs/prefs_helper.dart
+++ lib/shared_prefs/prefs_helper.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
-class PrefsHelper<T> {
+class PrefsHelper {
   static Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 
-  Future<void> set(String key, T value) async {
+  Future<void> set<T>(String key, T value) async {
     final p = await prefs;
     if (T is bool) {
       p.setBool(key, value as bool);
@@ -46,40 +43,40 @@ class PrefsHelper<T> {
   }
 
 
-  Future<T> get(String key) async {
-    final p = await prefs;
-    if (T is bool) {
-      return (p.getBool(key) ?? false) as T;
-    } else if (T is int) {
-      return (p.getInt(key) ?? 0) as T;
-    } else if (T is double) {
-      return (p.getDouble(key) ?? 0) as T;
-    } else if (T is String) {
-      if (key == 'locale') {
-        return (p.getString(key) ?? "ru") as T;
-      } else {
-        return (p.getString(key) ?? "") as T;
-      }
-    } else {
-      log("Prefs error for key $key");
-      throw UnimplementedError();
-    }
-  }
+  // Future<T> get(String key) async {
+  //   final p = await prefs;
+  //   if (T is bool) {
+  //     return (p.getBool(key) ?? false) as T;
+  //   } else if (T is int) {
+  //     return (p.getInt(key) ?? 0) as T;
+  //   } else if (T is double) {
+  //     return (p.getDouble(key) ?? 0) as T;
+  //   } else if (T is String) {
+  //     if (key == 'locale') {
+  //       return (p.getString(key) ?? "ru") as T;
+  //     } else {
+  //       return (p.getString(key) ?? "") as T;
+  //     }
+  //   } else {
+  //     log("Prefs error for key $key");
+  //     throw UnimplementedError();
+  //   }
+  // }
 
-  Future<R?> getClass<R>(String key, Function(T) converter) async {
-    final p = await prefs;
-    final value = p.getString(key) ?? '';
-    try {
-      return converter(value as T);
-    } catch (e) {
-      try {
-        final json = jsonDecode(value);
-        return converter(json as T);
-      } catch (e) {
-        return null;
-      }
-    }
-  }
+  // Future<R?> getClass<R>(String key, Function(T) converter) async {
+  //   final p = await prefs;
+  //   final value = p.getString(key) ?? '';
+  //   try {
+  //     return converter(value as T);
+  //   } catch (e) {
+  //     try {
+  //       final json = jsonDecode(value);
+  //       return converter(json as T);
+  //     } catch (e) {
+  //       return null;
+  //     }
+  //   }
+  // }
 
   Future clear() async {
     final p = await prefs;
