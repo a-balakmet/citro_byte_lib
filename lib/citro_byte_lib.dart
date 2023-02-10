7fe68b588ce400267343e1c0833079a2c7e0beec
andrey <a.balakhmet@gmail.com>
2023-02-10 11:57:42 +0600

    renaming

--- lib/citro_byte_lib.dart
+++ lib/citro_byte_lib.dart
@@ -2,7 +2,7 @@ library citro_byte_lib;
 
 import 'package:package_info_plus/package_info_plus.dart';
 
-class DataProvider {
+class CBDataProvider {
   Future<String> getVersion() async {
     final package = await PackageInfo.fromPlatform();
     return package.version;
