316b8d4b8c785c105c23f33ede8f958cb0b26a5a
andrey <a.balakhmet@gmail.com>
2023-02-11 21:17:45 +0600

    packageName added

--- lib/cb_data_provider.dart
+++ lib/cb_data_provider.dart
@@ -3,8 +3,13 @@ library cb_data_provider;
 import 'package:package_info_plus/package_info_plus.dart';
 
 class CBDataProvider {
-  Future<String> getVersion() async {
+  Future<String> getPackageVersion() async {
     final package = await PackageInfo.fromPlatform();
     return package.version;
+  }
+
+  Future<String> getPackageName() async {
+    final package = await PackageInfo.fromPlatform();
+    return package.packageName;
   }
 }
