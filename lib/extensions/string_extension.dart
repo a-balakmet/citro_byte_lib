6490e960e9a9be79ea71c351df6284eb9134cc8c
andrey <a.balakhmet@gmail.com>
2023-02-28 09:59:59 +0600

    Extension to convert string to phone format added.

--- lib/extensions/string_extension.dart
+++ lib/extensions/string_extension.dart
@@ -1,7 +1,8 @@
 import 'package:intl/intl.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:libphonenumber_plugin/libphonenumber_plugin.dart';
 
 extension Strings on String {
-
   DateTime stringToDate(String pattern) => DateFormat(pattern).parse(this);
 
   String yyyyMMdd(String pattern) {
@@ -40,5 +41,11 @@ extension Strings on String {
 
   String toFormattedPhoneNumber() {
     return replaceAllMapped(RegExp(r'(\d{3})(\d{3})(\d+)'), (Match m) => "${m[1]} ${m[2]} ${m[3]}");
+  }
+
+  Future<String> toPhoneFormat() async {
+    PhoneNumber number = await PhoneNumber.getRegionInfoFromPhoneNumber(this, 'RU');
+    String? formattedNumber = await PhoneNumberUtil.formatAsYouType(number.phoneNumber!, number.isoCode!);
+    return formattedNumber ?? this;
   }
 }
