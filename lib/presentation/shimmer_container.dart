7d9f8c4a3e637259f2bd9a65b3b69059c8e04777
Andrey <61378523+a-balakmet@users.noreply.github.com>
2023-04-28 14:41:24 +0600

    Spelling

--- lib/presentation/shimmer_container.dart
+++ lib/presentation/shimmer_container.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ShimmerContainer extends StatelessWidget {
-  final Widget widget;
+  final Widget child;
   final Color? baseColor;
   final Color? highlightColor;
-  const ShimmerContainer({Key? key, required this.widget, this.baseColor, this.highlightColor}) : super(key: key);
+  const ShimmerContainer({Key? key, required this.child, this.baseColor, this.highlightColor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Shimmer.fromColors(
       baseColor: baseColor ?? Colors.grey.shade500,
       highlightColor: highlightColor ?? Colors.white,
-      child: widget,
+      child: child,
     );
   }
 }
